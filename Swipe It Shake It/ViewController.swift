42c52217d64bb52ec5ed549735c3a1dcd6a043a4
Jared Allen <jarmallen@gmail.com>
2016-05-06 12:50:37 -0700

    Initial Commit

---
+++ Swipe It Shake It/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swipe It Shake It
+//
+//  Created by Jared Allen on 5/6/16.
+//  Copyright © 2016 Jared Allen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
