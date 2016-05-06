9722eded602fcd76be7c42bf091baa4c2fc6997c
Jared Allen <jarmallen@gmail.com>
2016-05-06 12:57:31 -0700

    Commit again

--- Swipe It Shake It/ViewController.swift
+++ Swipe It Shake It/ViewController.swift
@@ -7,13 +7,88 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(_:)))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(_:)))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(_:)))
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        self.view.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(_:)))
+        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+        self.view.addGestureRecognizer(swipeDown)
+
     }
+    
+    func swiped(gesture: UISwipeGestureRecognizer){
+        
+        var audioFilePath = String()
+        
+        if let swipeGesture: UISwipeGestureRecognizer = gesture {
+            
+            switch swipeGesture.direction {
+                
+            case UISwipeGestureRecognizerDirection.Right: audioFilePath = NSBundle.mainBundle().pathForResource("quick_buzzer_like_a_game_show_wrong_answer_", ofType: "mp3")!
+                
+            case UISwipeGestureRecognizerDirection.Left: audioFilePath = NSBundle.mainBundle().pathForResource("boing_comical_accent", ofType: "mp3")!
+                
+            case UISwipeGestureRecognizerDirection.Up: audioFilePath = NSBundle.mainBundle().pathForResource("comedy_accent_crash_and_burn", ofType: "mp3")!
+                
+            case UISwipeGestureRecognizerDirection.Down: audioFilePath = NSBundle.mainBundle().pathForResource("comedy_zips_pops_and_bonks", ofType: "mp3")!
+                
+            default:
+                break
+            }
+        }
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioFilePath))
+            
+            player.play()
+            
+        } catch {
+            
+            // Process error here
+            
+        }
+    }
+    
+        override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+            if event?.subtype == UIEventSubtype.MotionShake {
+                
+                let audioFilePath = NSBundle.mainBundle().pathForResource("comedy_male_yelling_yee_ha", ofType: "mp3")!
+                
+                do {
+                    
+                    try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioFilePath))
+                    
+                    player.play()
+                    
+                } catch {
+                    
+                    // Process error here
+                    
+                }
+                
+                
+            }
+        }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
